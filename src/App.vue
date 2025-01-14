d1d2d1c435fcb1f282e26e35b6c248e16df53aa4
Jacob Vega <jacob.vega@3it.cl>
2025-01-14 16:46:09 -0300

    Primera versión del frontend

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>¡Hola, Bienvenido!</h1>
+    <p>Sistema de Administración de Estudiantes</p>
+    <MiComponente />
+  </div>
+</template>
+
+<script>
+import MiComponente from './components/MiComponente.vue'
+
+export default {
+  components: {
+    MiComponente,
+  },
+}
+</script>
